21550c4015a68683288c211ab278d8bf379e92e0
mathcube <mathcube7@gmail.com>
2022-11-05 08:01:47 +0100

    v0.0.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ description = 'Collection of useful Python tools'
 name = 'mathcube'
 year = '2022'
 url = 'https://github.com/mathcube7/mathcube'
-version = '0.0.2'
+version = '0.0.3'
 
 setup(
     name=name,
